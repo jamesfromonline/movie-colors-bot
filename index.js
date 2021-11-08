cf4130cc51eead9197e59e9da82ee6238e038b01
jamesfromonline <jtwebco@gmail.com>
2021-11-08 08:27:09 -0500

    fixes

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ const updateStatus = (mediaIds, status) => {
           usedIds.push(movieId);
           fs.writeFileSync("movies.json", JSON.stringify({ ids: usedIds }));
           console.log("tweet sent");
-          process.exit(-1)
+          process.exit()
         }
       });
     } else {
